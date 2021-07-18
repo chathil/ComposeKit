eb91f89bbaeb2a527f7dbe570ee23032a6076aea
Abdul Chathil <chathil98@gmail.com>
2021-07-18 14:47:37 +0700

    fix tests error

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,29 +39,23 @@ subprojects {
 //            licenseHeaderFile rootProject.file('spotless/copyright.kt') /* add copyright header */
         }
     }
+}
 
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            // Treat all Kotlin warnings as errors
-            allWarningsAsErrors = true
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        // Treat all Kotlin warnings as errors
+        allWarningsAsErrors = true
 
-            freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
 
-            // Enable experimental coroutines APIs, including Flow
-            freeCompilerArgs =
-                freeCompilerArgs + "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
-            freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlinx.coroutines.FlowPreview"
-            freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.Experimental"
+        // Enable experimental coroutines APIs, including Flow
+        freeCompilerArgs =
+            freeCompilerArgs + "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlinx.coroutines.FlowPreview"
+        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.Experimental"
 
-            // Set JVM target to 1.8
-            jvmTarget = "1.8"
-        }
-    }
-    // androidx.test and hilt are forcing JUnit, 4.12. This forces them to use 4.13
-    configurations.configureEach {
-        resolutionStrategy {
-            force(Libs.junit)
-        }
+        // Set JVM target to 1.8
+        jvmTarget = "1.8"
     }
 }
 
